4333b5a23f323f2db5c9af4512ee45ca4e7472d1
aliuskams3 <alyuskamaestre@gmail.com>
2023-11-21 17:28:52 -0400

    first commit

---
+++ Library_V1.1/inc/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer full-reset">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <h4 class="all-tittles">Acerca de</h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos a voluptatibus quae odio. Fugiat omnis dolores totam quos debitis ratione corrupti! Repudiandae quia sapiente repellendus ab consequatur eum blanditiis dolorum.
+                </p>
+            </div>
+       
+        </div>
+    </div>
+    <div class="footer-copyright full-reset all-tittles">© Todos los derechos reservados</div>
+</footer>
